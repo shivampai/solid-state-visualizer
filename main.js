0669d78a26007b4073e3b6ef84ae8eb7188484e0
Shivam Pai <72920537+shivampai@users.noreply.github.com>
2026-06-23 22:30:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-function drawPlane1() {
-    //AB
-    for (y = 0; y <= 3; y++) {
-        if ((y % 2) == 0) {
-            for (z = -2; z <= 1; z++) {
-                for (x = -2; x <= 1; x++) {
-                    Plot3D.sphere(x * 2 + 1, y * Math.SQRT2, z * 2 + 1, 1)
+function drawPlane(planetype = "AB") {
+    Plot3D.clearAll()
+    if (planetype == 'AB') {
+        for (y = 0; y <= 3; y++) {
+            if ((y % 2) == 0) {
+                for (z = -2; z <= 1; z++) {
+                    for (x = -2; x <= 1; x++) {
+                        Plot3D.sphere(x * 2 + 1, y * Math.SQRT2, z * 2 + 1, 1)
+                    }
+                }
+            } else {
+                for (z = -1; z <= 1; z++) {
+                    for (x = -1; x <= 1; x++) {
+                        Plot3D.sphere(x * 2, y * Math.SQRT2, z * 2, 1, 0x0000ff)
+                    }
                 }
             }
-        } else {
-            for (z = -1; z <= 1; z++) {
-                for (x = -1; x <= 1; x++) {
-                    Plot3D.sphere(x * 2, y * Math.SQRT2, z * 2, 1, 0x0000ff)
+        }
+    } else if (planetype == 'AA') {
+        for (y = 0; y <= 3; y++) {
+            for (z = -2; z <= 1; z++) {
+                for (x = -2; x <= 1; x++) {
+                    Plot3D.sphere(x * 2 + 1, y * 2, z * 2 + 1, 1)
                 }
             }
         }
     }
+
 }
