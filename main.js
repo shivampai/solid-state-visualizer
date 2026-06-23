31ea883251582b22a162c10756ba738e1ae77582
Shivam Pai <72920537+shivampai@users.noreply.github.com>
2026-06-23 22:14:50 +0530

    Refactor drawPlane1 function for improved structure

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 function drawPlane1() {
-    // for (y = 1; y <= 3; y++) {
-        for (z = -2; z <= 2; z++) {
-            for (x = -2; x <= 2; x++) {
-                Plot3D.sphere(x * 2, 0, z * 2, 1, 0x0000ff)
+    //AB
+    for (y = 0; y <= 3; y++) {
+        if ((y % 2) == 0) {
+            for (z = -2; z <= 1; z++) {
+                for (x = -2; x <= 1; x++) {
+                    Plot3D.sphere(x * 2 + 1, y * Math.SQRT2, z * 2 + 1, 1)
+                }
             }
-        }
-    // }
-    for (z = -3; z <= 2; z++) {
-        for (x = -3; x <= 2; x++) {
-            Plot3D.sphere(x * 2 + 1, Math.SQRT2, z * 2 + 1, 1)
+        } else {
+            for (z = -1; z <= 1; z++) {
+                for (x = -1; x <= 1; x++) {
+                    Plot3D.sphere(x * 2, y * Math.SQRT2, z * 2, 1, 0x0000ff)
+                }
+            }
         }
     }
 }
